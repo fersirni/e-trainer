d00097bfc433e47eff228634eb37156070e2b868
fpsirnideb <fpsirni@debmedia.com>
2021-03-12 17:02:24 -0300

    add register page

---
+++ web/src/components/InputField.tsx
@@ -0,0 +1,34 @@
+import React, { InputHTMLAttributes } from "react";
+import { useField } from "formik";
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  FormErrorMessage,
+} from "@chakra-ui/react";
+
+type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
+  name: string;
+  label: string;
+  placeholder: string;
+};
+
+export const InputField: React.FC<InputFieldProps> = ({
+  label,
+  size: _, // underscore to rename useless variable
+  ...props
+}) => {
+  const [field, { error }] = useField(props);
+  return (
+    <FormControl isInvalid={!!error}>
+      <FormLabel htmlFor={field.name}>{label}</FormLabel>
+      <Input
+        {...field}
+        {...props}
+        id={field.name}
+        placeholder={props.placeholder}
+      />
+      {error ? <FormErrorMessage>{error}</FormErrorMessage> : null}
+    </FormControl>
+  );
+};
--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -1,38 +1,47 @@
 import React from "react";
 import { Formik, Form } from "formik";
+import { Wrapper } from "../components/Wrapper";
+import { InputField } from "../components/InputField";
 import {
   FormControl,
   FormLabel,
   Input,
   FormErrorMessage,
+  Box,
+  Button,
 } from "@chakra-ui/react";
-import { valueScaleCorrection } from "framer-motion/types/render/dom/projection/scale-correction";
 
 interface registerProps {}
 
 const Register: React.FC<registerProps> = ({}) => {
   return (
-    <Formik
-      initialValues={{ username: "", password: "" }}
-      onSubmit={(values) => {
-        console.log(values);
-      }}
-    >
-      {({ values, handleChange }) => (
-        <Form>
-          <FormControl>
-            <FormLabel htmlFor="username">Username</FormLabel>
-            <Input
-              value={values.username}
-              id="username"
-              placeholder="username"
-              onChange={handleChange}
-            />
-            {/* <FormErrorMessage>{form.errors.name}</FormErrorMessage> */}
-          </FormControl>
-        </Form>
-      )}
-    </Formik>
+    <Wrapper variant='small'>
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+              <InputField
+                name="username"
+                label="Username"
+                placeholder="username"
+              />
+              <Box mt={4}>
+                <InputField
+                  name="password"
+                  label="Password"
+                  placeholder="password"
+                  type="password"
+                />
+              </Box>
+              <Button mt={4} type="submit" isLoading={isSubmitting} colorScheme="teal">Register</Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
   );
 };
 
